2df65bed2347d36786e4ff5ccd7957a844afd475
Jeremy <xdetroit@live.com>
2019-08-02 10:12:54 -0400

    changed shuffle function to fisher yates shuffle

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-let cards = [];
+let cards = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 
 function startTimer() {
   let seconds = 0;
@@ -18,11 +18,20 @@ function startGame() {
   startTimer();
 }
 
-function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 18);
-    card.style.order = randomPos;
-  });
+function shuffle(cards) {
+  var currentIndex = cards.length,
+    temporaryValue,
+    randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = cards[currentIndex];
+    cards[currentIndex] = cards[randomIndex];
+    cards[randomIndex] = temporaryValue;
+  }
+  return cards;
 }
 
 document.querySelector("#start-btn").addEventListener("click", startGame);
+console.log(shuffle(cards));
