43b83086769e20a6aa8e0509ba28adbe4f4b5f46
Asia Davis <asia.davis369@gmail.com>
2019-08-02 10:49:35 -0400

    Created the flip function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+const cards = document.querySelectorAll(".memory-card");
+let hasFlipped = false;
+let firstCard, secondCard;
+function flipCard() {
+  this.classList.add(`flip`);
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+
+    console.log({ hasFlippedCard, firstCard });
+  }
+}
+cards.forEach(card => card.addEventListener(`click`, flipCard));
+
 function startTimer() {
   let seconds = 0;
   timer = setInterval(function() {
