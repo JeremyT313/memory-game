21be84575ba6c3e7a5828df4a827fdeafdd1e892
kody <kodypeters91@gmail.com>
2019-08-02 11:07:08 -0400

    Created structure for matching cards function

--- js/script.js
+++ js/script.js
@@ -10,3 +10,31 @@ function startTimer() {
 function stopTimer() {
   clearInterval(timer);
 }
+
+endGameCounter = deck.length;
+
+function isMatch(card1, card2) {
+  if (card1.getAttribute("data-matchValue" === card2.getAttribute("data-matchValue"))) {
+    //run code confirming a match
+    //code points to removeMatchedCards function
+    //removeMatchedCards(card1, card2);
+    endGameCounter -= 2;
+  } else {
+    //run code to flip cards back over and continue game
+    // function flipCard()
+  }
+  isEndGame(endGameCounter);
+
+}
+
+function removeMatchedCards(card1, card2) {
+  //removes the matched cards from teh play area
+
+
+}
+
+function isEndGame(counter) {
+  //checks for End of the Game
+  //counter counts the cards that are taken out until it reaches the number
+  //of total cards in a cam
+}
